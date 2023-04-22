41d0d9f7d0c1de1f4257ce14130ec88c4b34e931
Rayssa <raycadilhe@hotmail.com>
2023-04-22 16:13:09 -0300

    att rotas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,12 @@
 // Importando o packages
 import express from 'express'
+import cors from 'cors'
 
 // instanciando o servidor
 const app = express()
+
+
+app.use(cors());
 
 // configurando o servidor para receber requisições com o corpo no formato JSON
 app.use(express.json())
