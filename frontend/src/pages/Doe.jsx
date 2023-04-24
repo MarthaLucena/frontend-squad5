3e2df0fa02787bb9307ee068e1c800279ceeac41
Rayssa Cadilhe <62357420+raycadilhe@users.noreply.github.com>
2023-04-23 22:50:43 -0300

    att doações

--- frontend/src/pages/Doe.jsx
+++ frontend/src/pages/Doe.jsx
@@ -1,18 +1,17 @@
-/*
-
-import Banner from "../assets/components/Banner/Banner"
 import Navbar from "../assets/components/Navbar/Navbar"
 import Footer from "../assets/components/Footer/Footer"
-
-function Noticias () {
-    return(
+import Doacao from "../assets/components/Doacao/Doacao"
+import Banner from "../assets/components/Banner/Banner"
+function Doe() {
+    return (
         <div>
-<Navbar />
-        <Banner />
-        <Footer />
+            <Navbar />
+            <Banner />
+            <Doacao />
+            <Footer />
         </div>
     )
-    
+
 }
 
-export default Noticias */
+export default Doe 
