db0897d9c4739f3bbce060a13a00dff0b6f7a27e
Danilo Santos <116808289+DSantos29@users.noreply.github.com>
2023-04-24 11:36:46 -0300

    Adicionando o Painel de Usuários e Admins

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Banner from "./assets/components/Banner/Banner";
-import Navbar from "./assets/components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
-import Contato from "./pages/Contato";
-import Noticias from "./pages/Noticias";
-import Doe from "./pages/Doe";
-
-/* ===== Imports estão comentados para não dar erro. Ao inserir os componentes nas pages, tira o comentário do import!
+import Contato from "./pages/Contato"
 
 import Comunidades from './pages/Comunidades.jsx'
 import ImpactoSocial from './pages/ImpactoSocial.jsx'
 import Noticias from './pages/Noticias.jsx'
 import Projetos from './pages/Projetos.jsx'
- 
+
 import Login from './pages/Login.jsx'
-*/
+import Cadastro from "./pages/Cadastro.jsx";
+import IPerfil from "./pages/IPerfil.jsx";
+import { AuthProvider } from "../../Context/AuthContext";
+
 function app() {
   return (
     <div className="App">
+      <AuthProvider>
       <Router>
         <Switch>
           <Route exact path="/">
@@ -33,24 +31,27 @@ function app() {
             <Contato />
           </Route>
 
-          <Route path="/noticias">
-            <Noticias />
-          </Route>
-
-     <Route path="/doe">
-            <Doe />
-          </Route>
-   
           {/* ===== As rotas relacionadas as imports de cima também estão comentadas para evitar erro.
-          Conforme as Pages forem configuradas, tirar o comentário
+          Conforme as Pages forem configuradas, tirar o comentário */}
 
- 
+        
+        <Route path="/noticias">
+          <Noticias />
+        </Route>
+
+
+        <Route path="/perfil">
+          <IPerfil />
+        </Route>
         <Route path="/login">
           <Login />
         </Route>
-        */}
+        <Route path="/cadastro">
+          <Cadastro />
+        </Route>
         </Switch>
       </Router>
+      </AuthProvider>
     </div>
   );
 }
