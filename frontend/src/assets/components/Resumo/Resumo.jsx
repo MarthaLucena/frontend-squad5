492d27aac00ed60b150743b0d0e28338c219118e
Rayssa Cadilhe <62357420+raycadilhe@users.noreply.github.com>
2023-04-25 17:22:23 -0300

    últimas atts

--- frontend/src/assets/components/Resumo/Resumo.jsx
+++ frontend/src/assets/components/Resumo/Resumo.jsx
@@ -7,9 +7,7 @@ function Resumo() {
                     Vamos espalhar a esperança e o otimismo sobre o futuro da ZN <strong>juntos.</strong>
                 </p>
             </div>
-            <div className="linha">
-
-            </div>
+            <div className="linha" />
             <div className="textoInicial">
             Alô Comunidades é um projeto dedicado a destacar as histórias e eventos que muitas vezes são ignorados pelos meios de comunicação tradicionais. Buscamos trazer uma nova perspectiva sobre as comunidades e seus moradores. Queremos inspirar as pessoas a se unirem, apoiarem uns aos outros e trabalharem juntas para construir um futuro melhor.
 <br/><strong>Acreditamos que, ao compartilhar essas histórias, podemos inspirar as pessoas a se envolverem e fazerem a diferença em suas próprias comunidades.</strong>
